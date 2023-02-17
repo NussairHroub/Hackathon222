25477ad2bb341f3a4fc4a1ab084901c463904b2c
AhmadAlhaid <ahmed.alhaid2002@gmail.com>
2023-02-17 16:57:54 +0300

    add models

--- kfupm_clubs/lib/models/user.dart
+++ kfupm_clubs/lib/models/user.dart
@@ -1,5 +1,12 @@
 class User {
   final String id;
+  final String name;
+  final String email;
+  final String level;
 
-  User({required this.id});
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.level});
 }
